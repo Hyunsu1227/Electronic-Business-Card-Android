a8b080628f0173dc9fadc294beeb795bea92f4ff
Hyunsu <hyunsujang1227@gmail.com>
2020-11-08 02:02:05 +0900

    init android app

---
+++ ElectronicBusinessCard/app/src/main/java/com/example/electronic_business_card/UI/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.electronic_business_card.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electronic_business_card.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
